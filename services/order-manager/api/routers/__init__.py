7bda3d79677397695975503b061134fc6dc94faa
Godjoberto04 <plaidissimo@gmail.com>
2025-03-12 19:46:09 +0100

    Ajout du fichier d'initialisation des routeurs

---
+++ services/order-manager/api/routers/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Routeurs pour l'API de l'agent Order Manager
+Fait partie du projet Dropshipping Crew AI
+"""
+
+from . import health, orders, supplier_orders
+
+__all__ = ['health', 'orders', 'supplier_orders']
