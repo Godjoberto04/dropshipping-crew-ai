1d0b091017519271ae3f8c16f39f48a9ae1a2c9f
Godjoberto04 <plaidissimo@gmail.com>
2025-03-13 18:50:45 +0100

    Ajout du module d'analyse des marketplaces

---
+++ services/data-analyzer/data_sources/marketplaces/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module d'analyse des marketplaces pour extraire les données de produits concurrents.
+"""
+
+from .marketplace_analyzer import MarketplaceAnalyzer
+
+__all__ = ["MarketplaceAnalyzer"]
