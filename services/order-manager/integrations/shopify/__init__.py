66d911671a1e283e5e30d07e51f4214c868c0149
Godjoberto04 <plaidissimo@gmail.com>
2025-03-13 19:08:03 +0100

    Ajout du module d'intégration Shopify

---
+++ services/order-manager/integrations/shopify/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Initialisation du module d'intégration Shopify
+Fait partie du projet Dropshipping Crew AI
+"""
+
+from .client import ShopifyClient
+
+__all__ = ['ShopifyClient']
