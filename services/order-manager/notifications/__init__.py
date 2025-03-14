42aaf2da56451462910909d35260cceb50f0810b
Godjoberto04 <plaidissimo@gmail.com>
2025-03-14 16:59:21 +0100

    Ajout du module de notifications pour l'agent Order Manager

---
+++ services/order-manager/notifications/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module de notification pour l'agent Order Manager.
+Ce module contient les différentes méthodes de notification utilisées par l'agent.
+"""
+
+from .notification_manager import NotificationManager, NotificationType
+
+__all__ = ['NotificationManager', 'NotificationType']
