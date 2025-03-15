9a0cce0c2e23dedb87db85ea80d301b9fd5ed4fd
Godjoberto04 <plaidissimo@gmail.com>
2025-03-15 11:53:19 +0100

    Mise à jour du fichier d'initialisation des intégrations

--- services/order-manager/integrations/__init__.py
+++ services/order-manager/integrations/__init__.py
@@ -1,1 +1,13 @@
-# Integrations package
+#!/usr/bin/env python3
+"""
+Initialisation du module d'intégrations
+Fait partie du projet Dropshipping Crew AI
+"""
+
+from .shopify import ShopifyClient
+from .suppliers import SupplierCommunicator
+
+__all__ = [
+    'ShopifyClient',
+    'SupplierCommunicator'
+]
