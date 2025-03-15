379592c5d6c7eff10353c5d208b6396a656d44f0
Godjoberto04 <plaidissimo@gmail.com>
2025-03-15 11:54:02 +0100

    Mise à jour de l'initialisation des modèles

--- services/order-manager/models/__init__.py
+++ services/order-manager/models/__init__.py
@@ -1,1 +1,23 @@
-# Models package
+#!/usr/bin/env python3
+"""
+Modèles pour l'agent Order Manager
+Fait partie du projet Dropshipping Crew AI
+"""
+
+from .order import Order, OrderStatus, OrderLineItem
+from .supplier_order import SupplierOrder, SupplierOrderStatus
+from .shipping import ShippingInfo, TrackingInfo
+
+# Types de fournisseurs supportés par l'agent Order Manager
+class SupplierType:
+    """
+    Types de fournisseurs supportés par l'agent Order Manager.
+    """
+    ALIEXPRESS = "aliexpress"
+    CJ_DROPSHIPPING = "cjdropshipping"
+    # Ajouter d'autres fournisseurs au besoin
+    
+    @classmethod
+    def get_all(cls):
+        """Retourne la liste de tous les types de fournisseurs supportés."""
+        return [getattr(cls, attr) for attr in dir(cls) if not attr.startswith("_") and attr.isupper()]
